7b8ffa171667c8ab2f19231119f540c626640af6
Aleksey <134486666+Aleky63@users.noreply.github.com>
2026-03-21 13:18:55 +0300

    01_21.03

--- js/main.js
+++ js/main.js
@@ -10,19 +10,18 @@ document
     document.getElementById("my-modal").classList.remove("open");
   });
 
-
-  window.addEventListener('keydown', (e) => {
-    if (e.key === "Escape") {
-        document.getElementById("my-modal").classList.remove("open")
-    }
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    document.getElementById("my-modal").classList.remove("open");
+  }
 });
 
- 
-
-  document.querySelector("#my-modal .modal__box").addEventListener('click', event => {
+document
+  .querySelector("#my-modal .modal__box")
+  .addEventListener("click", (event) => {
     event._isClickWithInModal = true;
-});
-document.getElementById("my-modal").addEventListener('click', event => {
-    if (event._isClickWithInModal) return;
-    event.currentTarget.classList.remove('open');
+  });
+document.getElementById("my-modal").addEventListener("click", (event) => {
+  if (event._isClickWithInModal) return;
+  event.currentTarget.classList.remove("open");
 });
